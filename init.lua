0bf64ed2c59ccfef698bd0a135fe247895dfe470
Lukman Nulhakim <lookman.nh@gmail.com>
2025-11-09 14:19:23 +0700

    add auto install bootstrapper

--- init.lua
+++ init.lua
@@ -1,2 +1,32 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- ===============================
+-- Bootstrap Lazy.nvim
+-- ===============================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("Installing Lazy.nvim...")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ===============================
+-- Load Lazy and your plugins
+-- ===============================
+local lazy = require("lazy")
+
+lazy.setup("config.lazy", {
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json", -- ensures exact plugin versions
+})
+
+-- ===============================
+-- Auto-sync plugins on first run
+-- ===============================
+if not vim.loop.fs_stat(vim.fn.stdpath("data") .. "/lazy/lazy.nvim") then
+  vim.cmd("Lazy sync")
+end
